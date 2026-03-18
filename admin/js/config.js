94030e67f1b5397fdb92360e8d15663cf91067d1
naresh7km <naresh7km@gmail.com>
2026-03-18 22:53:59 +0100

    Update config.js

--- admin/js/config.js
+++ admin/js/config.js
@@ -1,7 +1,5 @@
 // Configuration - update this for your deployment
 window.WEBCALL_CONFIG = {
   // Backend server URL (no trailing slash)
-  API_URL: window.location.hostname === 'localhost'
-    ? 'http://localhost:3000'
-    : window.location.origin.replace('-admin', '-server'),
+  API_URL: "https://webcall2-0.onrender.com",
 };
