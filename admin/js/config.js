686cd9e64b923b07df08bfe27337bd8d9358901a
naresh7km <naresh7km@gmail.com>
2026-03-19 14:09:37 +0100

    Update config.js

--- admin/js/config.js
+++ admin/js/config.js
@@ -1,5 +1,5 @@
 // Configuration - update this for your deployment
 window.WEBCALL_CONFIG = {
   // Backend server URL (no trailing slash)
-  API_URL: "https://webcall2-0.onrender.com",
+  API_URL: "https://webcall2backendserver.onrender.com",
 };
